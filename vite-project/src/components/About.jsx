10de07d88a3803c9fd31f722dd9d165c936084b9
Sagar Gurung <sgrgrg34@gmail.com>
2024-11-16 14:58:36 +0545

    aboutus

--- vite-project/src/components/About.jsx
+++ vite-project/src/components/About.jsx
@@ -1,13 +1,51 @@
-import React, { useContext } from 'react'
-import productContext
- from '../context/productContext'
- const About = () => {
-  const context = useContext(productContext)
-  const { product } = context
-  console.log("hello", product);
-  return (
-    <h4>this is about</h4>
-  )
+import React, { useContext, useEffect } from 'react'
+import productContext from '../context/productContext'
+import Mac from "../assets/images/mac.jpeg"
+
+const About = () => {
+    const context = useContext(productContext)
+    const { product } = context
+    console.log("hello", product);
+    
+
+    
+    // useEffect(() => {
+    //     update()
+    //     fetchApi()
+
+    // }, [])
+
+    return (
+        <>
+            <div className="container">
+                <div className="row">
+                    {/* <h4> this is about us component. my product name is: {product.name} and price:{product.price}</h4> */}
+
+                    <h4 className="our-product-title">
+                        Our Product
+                    </h4>
+                    { product.map((item) => {
+                        return (
+                            <div className='col-md-3'>
+                                <div key={item.id} className="card ">
+                                    <img src={Mac} className="card-img-top" alt="..." />
+                                    <div className="card-body">
+                                        <h5 className="card-title">{item.name}</h5>
+                                        <p className="card-text">{item.description}</p>
+                                        <p className="card-text">Rs. {item.price}</p>
+                                        <button className='btn btn-primary'>Add to cart</button>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+
+                    })}
+
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default About
+
